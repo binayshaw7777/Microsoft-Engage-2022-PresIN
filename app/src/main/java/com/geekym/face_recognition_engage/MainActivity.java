6c31ba37cdd0d808c93b06ec4415cce578d46e3b
Binay Shaw <binayshaw7777@gmail.com>
2022-05-06 20:23:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/geekym/face_recognition_engage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geekym.face_recognition_engage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
