766bfd4793ba7b9b79f0ffc50d62dca5bbba766f
Binay Shaw <binayshaw7777@gmail.com>
2022-05-11 23:15:21 +0530

    Name Display in result 🔥

--- app/src/main/java/com/geekym/face_recognition_engage/Attendance_Result_Activity.java
+++ app/src/main/java/com/geekym/face_recognition_engage/Attendance_Result_Activity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Attendance_Result_Activity extends AppCompatActivity {
 
     Button Home;
     TextView Name;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,21 @@ public class Attendance_Result_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_attendance_result);
         Initialize();
 
-        Name.setText("USERNAME HERE");
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() { //To display user's data in card view
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users userprofile = snapshot.getValue(Users.class);
+                if (userprofile != null) {
+                    String name = userprofile.name;
+                    Name.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Home.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), HomeScreen.class)));
 
@@ -27,5 +54,8 @@ public class Attendance_Result_Activity extends AppCompatActivity {
     private void Initialize() {
         Home = findViewById(R.id.back_home);
         Name = findViewById(R.id.name_display);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
     }
 }
